0c91b52ff690786687a2076079278135b47d5041
Timofey Epanchintsev <epanchee@gmail.com>
2020-11-14 17:09:51 +0500

    Исправлено(тесты диска): при рефакторинге содержимое тестов перепуталось

--- check_pi_disk.sh
+++ check_pi_disk.sh
@@ -3,16 +3,11 @@
 # shellcheck source=/dev/null
 . "$(dirname "$0")/config"
 
-error_msg='Не получается записать данные на диск'
-info_msg='Теперь писать на диск опять можно'
+error_msg='Диск Toshiba на Raspberry Pi куда-то пропал'
+info_msg='Диск Toshiba восстановлен'
 
 check() {
   set -o pipefail
   ssh -o ConnectTimeout=2 root@"$PI_ADDR" df -h | grep -q "$FS_ROOT" && return 0
-  return 1
-}
-
-fix() {
-  ssh root@"$PI_ADDR" -o ConnectTimeout=2 mount -o remount,rw /dev/sda1 && return 0
   return 1
 }
--- check_toshiba_writable.sh
+++ check_toshiba_writable.sh
@@ -3,10 +3,15 @@
 # shellcheck source=/dev/null
 . "$(dirname "$0")/config"
 
-error_msg='Диск Toshiba на Raspberry Pi куда-то пропал'
-info_msg='Диск Toshiba восстановлен'
+error_msg='Не получается записать данные на диск'
+info_msg='Теперь писать на диск опять можно'
 
 check() {
   ssh -o ConnectTimeout=2 root@"$PI_ADDR" "touch $FS_ROOT/test && rm $FS_ROOT/test" && return 0
+  return 1
+}
+
+fix() {
+  ssh root@"$PI_ADDR" -o ConnectTimeout=2 mount -o remount,rw /dev/sda1 && return 0
   return 1
 }
