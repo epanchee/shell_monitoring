4614ce8aff69959e36c6a8cf7763bf47eda5f9fd
Timofey Epanchintsev <epanchee@gmail.com>
2020-11-14 17:37:15 +0500

    Добавлено(отправка сообщений в тг): теперь используется Google DNS
    Бывает так, что DNS сервер развернут на Raspberry Pi и если так, то
    нотифаи не будут работать.

--- tg_say.sh
+++ tg_say.sh
@@ -5,4 +5,4 @@
 
 MSG=${1:-Empty message}
 
-curl -s -ssl "https://api.telegram.org/bot$API_TOKEN/sendMessage?chat_id=$CHAT_ID&parse_mode=html&text=$MSG" &>/dev/null
+curl -s -ssl --dns-servers 8.8.8.8 "https://api.telegram.org/bot$API_TOKEN/sendMessage?chat_id=$CHAT_ID&parse_mode=html&text=$MSG" &>/dev/null
